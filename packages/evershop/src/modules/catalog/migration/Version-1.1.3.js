d28fd2464b75ebec6cc299339b56de203de07ec6
pnt <phamngocthan2001@gmail.com>
2025-10-01 23:51:46 +0700

    fix migration

--- packages/evershop/src/modules/catalog/migration/Version-1.1.3.js
+++ packages/evershop/src/modules/catalog/migration/Version-1.1.3.js
@@ -3,7 +3,6 @@ const { execute } = require('@evershop/postgres-query-builder');
 module.exports = exports = async (connection) => {
   await execute(
     connection,
-    `ALTER TABLE "account" ADD COLUMN order_id INT DEFAULT NULL;
-     ALTER TABLE "account" ADD CONSTRAINT "FK_ORDER_ACCOUNT" FOREIGN KEY (order_id) REFERENCES "order" (order_id) ON DELETE SET NULL;`
+    `ALTER TABLE "account" ADD COLUMN order_id INT DEFAULT NULL;`
   );
 }; 
