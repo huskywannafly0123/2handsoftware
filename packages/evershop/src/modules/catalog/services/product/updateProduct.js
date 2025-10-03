b4161efa797286511f5e6c383719c929eff9360d
huskywannafly0123 <128126309+huskywannafly0123@users.noreply.github.com>
2025-10-03 13:30:24 +0700

    revert release connection

--- packages/evershop/src/modules/catalog/services/product/updateProduct.js
+++ packages/evershop/src/modules/catalog/services/product/updateProduct.js
@@ -380,9 +380,7 @@ async function updateProduct(uuid, data, context) {
   } catch (e) {
     await rollback(connection);
     throw e;
-  }finally {
-      connection.release();
-    }
+  }
 }
 
 module.exports = async (uuid, data, context) => {
